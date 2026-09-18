b22cebeaf925b7ecb2968f755d58bb98e6809763
202627662 <turkmn.mrt@gmail.com>
2026-09-18 14:13:22 +0900

    3주차 수업 내용

---
+++ Week03/Sample03-1/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    boolean flag; //false;
+    byte value1 = -128;      // -128 ~ 127
+    short value2 = 32627;       //- 32,768 ~ + 32,627
+    int value3 = 444444444;
+    long value4 = 444444444466666666l;
+    double value5 = 4.5555555555555555555;
+    float value6 = 4.555555555555555555555f;
+    char value7 = 'A';
+    String value8 = "AB";
+}
---
+++ Week03/Sample03-6/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    String name = "Murat";
+    Float height = 178.5f;
+    Double weight = 62.5;
+    int age = 21;
+    char gender = '남';
+
+    System.out.printf("%s의 height %.1fcm.\n",name,height);
+    System.out.printf("%s의 weight %.1fkg.\n",name,weight);
+    System.out.printf("%s의 age %d살.\n",name,age);
+    System.out.printf("%s의 gender %c자.\n",name,gender);
+}
